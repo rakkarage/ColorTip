95cfbcfb0c110607f06d2e0ac29ad8a6e027f97e
Rakka Rage <rakkarage@gmail.com>
2026-04-18 20:23:27 -0400

    r

--- ColorTip.lua
+++ ColorTip.lua
@@ -1,17 +1,12 @@
 -- 🎨 ColorTip: Dynamic (class & reaction) tooltip name, border, and status bar.
 
-local _, ns = ...
-
-ns.ColorTip = {}
-local ColorTip = ns.ColorTip
-
 local COLOR_ALLIED_GUILD = { r = 1, g = 0.85, b = 0.1 }
 local COLOR_OTHER_GUILD = { r = 0.75, g = 0.6, b = 0.15 }
 
-ColorTip.lastR, ColorTip.lastG, ColorTip.lastB = 1, 1, 1
-ColorTip.lastRR, ColorTip.lastRG, ColorTip.lastRB = nil, nil, nil
+local lastColorR, lastColorG, lastColorB = 1, 1, 1
+local lastReactionR, lastReactionG, lastReactionB = nil, nil, nil
 
-function GetReactionColor(unit)
+local function GetReactionColor(unit)
 	local reaction = UnitReaction(unit, "player")
 	if reaction then
 		local c = FACTION_BAR_COLORS[reaction]
@@ -19,87 +14,87 @@ function GetReactionColor(unit)
 	end
 end
 
-function ColorTip:UpdateTooltipColors(tooltip)
-	if tooltip ~= GameTooltip then return end
+local function SetTooltipBorderColors(border, colorR, colorG, colorB, reactionR, reactionG, reactionB)
+	if not border then return end
+	if reactionR then
+		border.TopEdge:SetVertexColor(reactionR, reactionG, reactionB)
+		border.TopLeftCorner:SetVertexColor(reactionR, reactionG, reactionB)
+		border.TopRightCorner:SetVertexColor(reactionR, reactionG, reactionB)
+		border.BottomEdge:SetVertexColor(colorR, colorG, colorB)
+		border.BottomLeftCorner:SetVertexColor(colorR, colorG, colorB)
+		border.BottomRightCorner:SetVertexColor(colorR, colorG, colorB)
+		border.LeftEdge:SetGradient("VERTICAL", CreateColor(colorR, colorG, colorB), CreateColor(reactionR, reactionG, reactionB))
+		border.RightEdge:SetGradient("VERTICAL", CreateColor(colorR, colorG, colorB), CreateColor(reactionR, reactionG, reactionB))
+	else
+		border:SetBorderColor(colorR, colorG, colorB)
+	end
+end
 
-	local _, unit = tooltip:GetUnit()
+local function SetTooltipStatusBarColor(r, g, b)
+	if GameTooltipStatusBar then
+		local tex = GameTooltipStatusBar:GetStatusBarTexture()
+		if tex then tex:SetVertexColor(r, g, b) end
+	end
+end
+
+local function RepaintColors(tooltip)
+	if tooltip ~= GameTooltip then return end
+	local border = tooltip.NineSlice
+	local hasCustomColors = tooltip:GetAlpha() > 0 and (lastReactionR or (lastColorR ~= 1 or lastColorG ~= 1 or lastColorB ~= 1))
+	if not hasCustomColors then return end
+	SetTooltipBorderColors(border, lastColorR, lastColorG, lastColorB, lastReactionR, lastReactionG, lastReactionB)
+	SetTooltipStatusBarColor(lastColorR, lastColorG, lastColorB)
+end
+
+local function ApplyColors(tooltip, unit)
+	if tooltip ~= GameTooltip then return end
 	local border = tooltip.NineSlice
 
-	local isSecret = unit and issecretvalue(unit)
-	local isUnit = not isSecret and unit and UnitExists(unit)
+	if UnitIsPlayer(unit) then
+		local _, classId = UnitClass(unit)
+		if classId then lastColorR, lastColorG, lastColorB = GetClassColor(classId) end
+		lastReactionR, lastReactionG, lastReactionB = GetReactionColor(unit)
 
-	if isUnit then
-		if UnitIsPlayer(unit) then
-			local _, classId = UnitClass(unit)
-			if classId then self.lastR, self.lastG, self.lastB = GetClassColor(classId) end
-			self.lastRR, self.lastRG, self.lastRB = GetReactionColor(unit)
+		local class = classId and (LOCALIZED_CLASS_NAMES_MALE[classId] or LOCALIZED_CLASS_NAMES_FEMALE[classId])
+		local faction = UnitFactionGroup(unit)
+		local playerGuild = GetGuildInfo("player")
+		local unitGuild = GetGuildInfo(unit)
 
-			local class = classId and (LOCALIZED_CLASS_NAMES_MALE[classId] or LOCALIZED_CLASS_NAMES_FEMALE[classId])
-			local faction = UnitFactionGroup(unit)
-			local playerGuild = GetGuildInfo("player")
-			local unitGuild = GetGuildInfo(unit)
-
-			for i = 2, 6 do
-				local line = _G["GameTooltipTextLeft" .. i]
-				if line then
-					local text = line:GetText()
-					if text and not issecretvalue(text) then
-						if class and text:find(class, 1, true) then
-							line:SetTextColor(self.lastR, self.lastG, self.lastB)
-						elseif faction and text == faction then
-							line:SetTextColor(self.lastRR or self.lastR, self.lastRG or self.lastG, self.lastRB or self.lastB)
-						elseif unitGuild and text:find(unitGuild, 1, true) then
-							if playerGuild and unitGuild == playerGuild then
-								line:SetTextColor(COLOR_ALLIED_GUILD.r, COLOR_ALLIED_GUILD.g, COLOR_ALLIED_GUILD.b)
-							else
-								line:SetTextColor(COLOR_OTHER_GUILD.r, COLOR_OTHER_GUILD.g, COLOR_OTHER_GUILD.b)
-							end
+		for i = 2, 6 do
+			local line = _G["GameTooltipTextLeft" .. i]
+			if line then
+				local text = line:GetText()
+				if text and not issecretvalue(text) then
+					if class and text:find(class, 1, true) then
+						line:SetTextColor(lastColorR, lastColorG, lastColorB)
+					elseif faction and text == faction then
+						line:SetTextColor(lastReactionR or lastColorR, lastReactionG or lastColorG, lastReactionB or lastColorB)
+					elseif unitGuild and text:find(unitGuild, 1, true) then
+						if playerGuild and unitGuild == playerGuild then
+							line:SetTextColor(COLOR_ALLIED_GUILD.r, COLOR_ALLIED_GUILD.g, COLOR_ALLIED_GUILD.b)
+						else
+							line:SetTextColor(COLOR_OTHER_GUILD.r, COLOR_OTHER_GUILD.g, COLOR_OTHER_GUILD.b)
 						end
 					end
 				end
 			end
-		else
-			self.lastRR, self.lastRG, self.lastRB = nil, nil, nil
-			local r, g, b = GetReactionColor(unit)
-			if r then
-				self.lastR, self.lastG, self.lastB = r, g, b
-			else
-				self.lastR, self.lastG, self.lastB = 1, 1, 1
-			end
 		end
+	else
+		lastReactionR, lastReactionG, lastReactionB = nil, nil, nil
+		local r, g, b = GetReactionColor(unit)
+		lastColorR, lastColorG, lastColorB = r or 1, g or 1, b or 1
 	end
 
-	local isFading = tooltip:GetAlpha() > 0 and (self.lastRR or (self.lastR ~= 1 or self.lastG ~= 1 or self.lastB ~= 1))
+	GameTooltipTextLeft1:SetTextColor(lastReactionR or lastColorR, lastReactionG or lastColorG, lastReactionB or lastColorB)
 
-	if isUnit or isFading then
-		if border then
-			if self.lastRR then
-				border.TopEdge:SetVertexColor(self.lastRR, self.lastRG, self.lastRB)
-				border.TopLeftCorner:SetVertexColor(self.lastRR, self.lastRG, self.lastRB)
-				border.TopRightCorner:SetVertexColor(self.lastRR, self.lastRG, self.lastRB)
-				border.BottomEdge:SetVertexColor(self.lastR, self.lastG, self.lastB)
-				border.BottomLeftCorner:SetVertexColor(self.lastR, self.lastG, self.lastB)
-				border.BottomRightCorner:SetVertexColor(self.lastR, self.lastG, self.lastB)
-				border.LeftEdge:SetGradient("VERTICAL", CreateColor(self.lastR, self.lastG, self.lastB), CreateColor(self.lastRR, self.lastRG, self.lastRB))
-				border.RightEdge:SetGradient("VERTICAL", CreateColor(self.lastR, self.lastG, self.lastB), CreateColor(self.lastRR, self.lastRG, self.lastRB))
-			else
-				border:SetBorderColor(self.lastR, self.lastG, self.lastB)
-			end
-		end
-
-		GameTooltipTextLeft1:SetTextColor(self.lastRR or self.lastR, self.lastRG or self.lastG, self.lastRB or self.lastB)
-
-		if GameTooltipStatusBar then
-			local tex = GameTooltipStatusBar:GetStatusBarTexture()
-			if tex then tex:SetVertexColor(self.lastR, self.lastG, self.lastB) end
-		end
-	end
+	SetTooltipBorderColors(border, lastColorR, lastColorG, lastColorB, lastReactionR, lastReactionG, lastReactionB)
+	SetTooltipStatusBarColor(lastColorR, lastColorG, lastColorB)
 end
 
-function ColorTip:ForceReset(tooltip)
+local function ForceReset(tooltip)
 	if tooltip ~= GameTooltip then return end
-	self.lastR, self.lastG, self.lastB = 1, 1, 1
-	self.lastRR, self.lastRG, self.lastRB = nil, nil, nil
+	lastColorR, lastColorG, lastColorB = 1, 1, 1
+	lastReactionR, lastReactionG, lastReactionB = nil, nil, nil
 	local border = tooltip.NineSlice
 	if border then border:SetBorderColor(1, 1, 1) end
 	if GameTooltipStatusBar then
@@ -108,10 +103,16 @@ function ColorTip:ForceReset(tooltip)
 	end
 end
 
-TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Unit, function(...) ColorTip:UpdateTooltipColors(...) end)
-GameTooltip:HookScript("OnUpdate", function(...) ColorTip:UpdateTooltipColors(...) end)
+TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Unit, function(tooltip, data)
+	local unit = (data and data.unitToken) or (UnitExists("mouseover") and "mouseover")
+	if not unit then return end
+	ApplyColors(tooltip, unit)
+end)
 
-TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Item, function(...) ColorTip:ForceReset(...) end)
-TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Spell, function(...) ColorTip:ForceReset(...) end)
-TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Action, function(...) ColorTip:ForceReset(...) end)
-TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Object, function(...) ColorTip:ForceReset(...) end)
+GameTooltip:HookScript("OnUpdate", RepaintColors)
+GameTooltip:HookScript("OnHide", ForceReset)
+
+TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Item, ForceReset)
+TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Spell, ForceReset)
+TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Action, ForceReset)
+TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Object, ForceReset)
